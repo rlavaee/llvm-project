70e53a8879a7880c3dc7100af8f3f5f68d333505
Rahman Lavaee <rahmanl@google.com>
2024-12-04 06:19:08 +0000

    Add dbgs in lld.

--- lld/ELF/Relocations.cpp
+++ lld/ELF/Relocations.cpp
@@ -819,8 +819,10 @@ void elf::reportUndefinedSymbols() {
 // Returns true if the undefined symbol will produce an error message.
 static bool maybeReportUndefined(Undefined &sym, InputSectionBase &sec,
                                  uint64_t offset) {
-  if (sym.getName().starts_with("__llvm_prefetch_target_"))
+  if (sym.getName().starts_with("__llvm_prefetch_target_")) {
+    dbgs() << "A prefetch for target: " << toString(sym) << " was rejected due to symbol undefined\n";
     return false;
+  }
   std::lock_guard<std::mutex> lock(relocMutex);
   // If versioned, issue an error (even if the symbol is weak) because we don't
   // know the defining filename which is required to construct a Verneed entry.
@@ -1261,8 +1263,10 @@ void RelocationScanner::processAux(RelExpr expr, RelType type, uint64_t offset,
     }
   }
 
-  if (sym.getName().starts_with("__llvm_prefetch_target_"))
+  if (sym.getName().starts_with("__llvm_prefetch_target_")) {
+    dbgs() << "A prefetch for target: " << toString(sym) << " was rejected due to relocation error\n";
     return;
+  }
   errorOrWarn("relocation " + toString(type) + " cannot be used against " +
               (sym.getName().empty() ? "local symbol"
                                      : "symbol '" + toString(sym) + "'") +
